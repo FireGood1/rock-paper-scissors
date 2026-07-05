da5f460fab070814014ecc41bb3778e694663c38
Firegood <mathhelperforoyu@gmail.com>
2026-07-05 23:57:34 +0000

    Created Round Functionality
    Created function playRound to represent a round of Rock Paper Scissors, where it takes the users input and computer's output to decide a winner for the round (or if they had a draw) and change their respective scores.

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ function getHumanChoice() {
     let answer = prompt("Choose Rock, Paper, or Scissors?").toUpperCase();
     return answer;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`Your choice was ${humanChoice} while the computer chose ${computerChoice}`)
+    if (humanChoice == "ROCK" && computerChoice == "SCISSORS" || humanChoice == "PAPER" && computerChoice == "ROCK" || humanChoice == "SCISSORS" && computerChoice == "PAPER") {
+        humanScore++;
+    }
+    else if (humanChoice == computerChoice) {
+    }
+    else {computerScore++;}
+    console.log("Your score is " + humanScore + "-" + computerScore + ".");
+}
+playRound(getComputerChoice(), getHumanChoice())
