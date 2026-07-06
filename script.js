4cf62bd9f686eb0c2ae6575e88a936db78c63ea6
Firegood <mathhelperforoyu@gmail.com>
2026-07-06 00:06:42 +0000

    Added 5 Rounds and Final win/tie/lose message.
    Added 5 separate rounds by repeating the function playRound 5 times with the new function playGame. Once 5 rounds are done, I coded a winning message depending on if the user wins, loses, or ties.

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    console.log(`Your choice was ${humanChoice} while the computer chose ${computerChoice}`)
+    console.log(`Your choice was ${humanChoice.toLowerCase()} while the computer chose ${computerChoice.toLowerCase()}.`)
     if (humanChoice == "ROCK" && computerChoice == "SCISSORS" || humanChoice == "PAPER" && computerChoice == "ROCK" || humanChoice == "SCISSORS" && computerChoice == "PAPER") {
         humanScore++;
     }
@@ -24,4 +24,20 @@ function playRound(humanChoice, computerChoice) {
     else {computerScore++;}
     console.log("Your score is " + humanScore + "-" + computerScore + ".");
 }
-playRound(getComputerChoice(), getHumanChoice())
+
+function playGame() {
+    for (x=0; x<5; x++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    if (humanScore > computerScore) {
+        console.log(`You won! Congratulations!`)
+    }
+    else if (humanScore == computerScore) {
+        console.log(`You tied! Maybe the next one will be a win!`)
+    }
+    else {
+        console.log(`You lost ): . Better luck next time!`)
+    }
+}
+
+playGame()
