151b70277d453faa210be6134e90f89bbcc28eb2
Firegood <mathhelperforoyu@gmail.com>
2026-07-12 01:57:35 +0000

    Rock Paper Scissors UI Update
    Added styles to organize text content, replaced console messages with ui (buttons) or text, and added event listeners for buttons to update scores.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+let humanScore = 0;
+let computerScore = 0;
+
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+let resultsContainer = document.querySelector("#results-container");
+let choice = document.createElement("p")
+let scoreboard = document.createElement("h2")
+let winner = document.createElement("h1")
+resultsContainer.appendChild(choice)
+resultsContainer.appendChild(scoreboard)
+resultsContainer.appendChild(winner);
+
+
 function getComputerChoice() {
     let randomChoice = Math.random();
     if (randomChoice <= 1/3) {randomChoice = "ROCK";}
@@ -5,39 +21,36 @@ function getComputerChoice() {
     else {randomChoice = "SCISSORS";}
     return randomChoice;
 }
-
-function getHumanChoice() {
-    let answer = prompt("Choose Rock, Paper, or Scissors?").toUpperCase();
-    return answer;
-}
-
-let humanScore = 0;
-let computerScore = 0;
+rock.addEventListener("click", () => {
+    if (humanScore < 5 && computerScore < 5) {
+        return playRound("ROCK", getComputerChoice());
+    }
+});
+paper.addEventListener("click", () => {
+    if (humanScore < 5 && computerScore < 5) {
+        return playRound("PAPER", getComputerChoice());
+    }
+});
+scissors.addEventListener("click", () => {
+    if (humanScore < 5 && computerScore < 5) {
+        playRound("SCISSORS", getComputerChoice());
+    }
+});
 
 function playRound(humanChoice, computerChoice) {
-    console.log(`Your choice was ${humanChoice.toLowerCase()} while the computer chose ${computerChoice.toLowerCase()}.`)
+
+    choice.textContent = `Your choice was ${humanChoice.toLowerCase()} while the computer chose ${computerChoice.toLowerCase()}.`
     if (humanChoice == "ROCK" && computerChoice == "SCISSORS" || humanChoice == "PAPER" && computerChoice == "ROCK" || humanChoice == "SCISSORS" && computerChoice == "PAPER") {
         humanScore++;
     }
-    else if (humanChoice == computerChoice) {
-    }
+    else if (humanChoice == computerChoice) {}
     else {computerScore++;}
-    console.log("Your score is " + humanScore + "-" + computerScore + ".");
-}
 
-function playGame() {
-    for (x=0; x<5; x++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    if (humanScore > computerScore) {
-        console.log(`You won! Congratulations!`)
-    }
-    else if (humanScore == computerScore) {
-        console.log(`You tied! Maybe the next one will be a win!`)
-    }
-    else {
-        console.log(`You lost ): . Better luck next time!`)
-    }
+    scoreboard.textContent = `The score is ${humanScore}-${computerScore}.`
+    if (humanScore == 5) {
+            winner.textContent = "You won! Congratulations!";
+        }
+    else if (computerScore == 5) {
+        winner.textContent = "You lost ):. Better luck next time!";
+     }  
 }
-
-playGame()
